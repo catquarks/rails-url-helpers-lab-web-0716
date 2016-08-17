77630e7204bc5a0847b6546082e5e2de729eab0f
Tami <catquarks@gmail.com>
2016-08-17 17:40:47 -0400

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,4 @@
+# binding.pry
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
   
@@ -6,9 +7,17 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
-  private
+  def activate_student
+    @student = self.set_student
+    @student.activate
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  # private
 
     def set_student
       @student = Student.find(params[:id])
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,18 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def activate
+  	if self.active == false
+  		self.active = true
+  	else
+  		self.active = false
+  	end
+  end
+
+  def active?
+  	self.active == true ? "active" : "inactive"
+  end
+
+
 end
